1bb17a3364cf9b4e9a037dabf7156cfdcd2d1a79
aaglis <aglissilva17@gmail.com>
2024-09-24 19:09:58 -0300

    feat(contact): complete contato (without response)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { HomeComponent } from './components/home/home.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { ProjectsComponent } from "./components/projects/projects.component";
 import { ExperiencesComponent } from './components/experiences/experiences.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, HomeComponent, SkillsComponent, ProjectsComponent, ExperiencesComponent],
+  imports: [HeaderComponent, HomeComponent, SkillsComponent, ProjectsComponent, ExperiencesComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/contact/contact.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { LucideAngularModule } from 'lucide-angular';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [LucideAngularModule],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.scss'
+})
+export class ContactComponent {
+
+}
