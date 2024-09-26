d3c12c52aa95c987f585a5d84ab862728c9dfff6
aaglis <aglissilva17@gmail.com>
2024-09-26 11:00:17 -0300

    feat(header): show header on scroll and fix bug on link home section

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { afterRender, Component, ElementRef, inject } from '@angular/core';
 import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
@@ -11,6 +11,31 @@ import { LucideAngularModule } from 'lucide-angular';
 })
 export class HeaderComponent {
   menuIsOpened: boolean = false;
+  private elementRef = inject(ElementRef)
+  lastScrollTop:number = 0
+
+  constructor() {
+    afterRender(() => {
+      window.addEventListener('scroll', () => {
+        if(scrollY > 200) {
+          this.elementRef.nativeElement.style.top = '-150px'
+        } else {
+          this.elementRef.nativeElement.style.top = '0'
+        }
+
+        if(scrollY > 400) {
+          this.elementRef.nativeElement.classList.add('shadow')
+        } else {
+          this.elementRef.nativeElement.classList.remove('shadow')
+        }
+
+        if((scrollY > 800) && (innerWidth > 1150)) {
+          this.elementRef.nativeElement.style.top = '20px'
+        }
+        this.lastScrollTop = scrollY
+      })
+    })
+  }
 
   toggleMenu() {
     this.menuIsOpened = !this.menuIsOpened;
